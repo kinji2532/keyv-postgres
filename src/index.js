ca810baf79fa19f4f12f34f821a06e7eadba3b94
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2021-04-19 05:04:45 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,26 +4,24 @@ const KeyvSql = require('@keyv/sql');
 const Client = require('pg').Client;
 
 class KeyvPostgres extends KeyvSql {
-	constructor(opts) {
-		opts = Object.assign({
-			dialect: 'postgres',
-			uri: 'postgresql://localhost:5432'
-		}, opts);
+  constructor(opts) {
+    opts = Object.assign({
+      dialect: 'postgres',
+      uri: 'postgresql://localhost:5432'
+    }, opts);
 
-		opts.connect = () => Promise.resolve()
-			.then(() => {
-                                const client = new Client({ connectionString: opts.uri, ssl: {rejectUnauthorized: false} });
-				client.connect();
-                                return sql => client.query(sql)
-					.then(data => {
-client.end();
-return data.rows
-}
-);
-			});
-
-		super(opts);
-	}
+    opts.connect = () => Promise.resolve()
+    .then(() => {
+      const client = new Client({ connectionString: opts.uri, ssl: {rejectUnauthorized: false} });
+      client.connect();
+      return sql => client.query(sql)
+      .then(data => {
+        client.end();
+        return data.rows
+      });
+    });
+    super(opts);
+  }
 }
 
 module.exports = KeyvPostgres;
